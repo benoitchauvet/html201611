b486ce245e44aa1edd798a3910223db3ac323f44
compay2k <compay2k@gmail.com>
2016-11-17 15:17:07 +0100

    jqskd

---
+++ www/js/utils.js
@@ -0,0 +1,20 @@
+﻿function creerArticle(conteneur, titre, teaser, imageUrl = 'images/licorne.bmp')
+{
+    let article = document.createElement("article");
+
+    let h2 = document.createElement("h2");
+    h2.innerHTML = titre;
+
+    let img = document.createElement("img");
+    img.setAttribute("src", imageUrl);
+    img.setAttribute("alt", "illustration");
+
+    let p = document.createElement("p");
+    p.innerHTML = teaser;
+
+    article.appendChild(h2);
+    article.appendChild(img);
+    article.appendChild(p);
+
+    conteneur.appendChild(article);
+}
